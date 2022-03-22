776a54f1c6cc19faaa2cba01cb4636ef18c4b049
Adeel Syed <adeel99@gmail.com>
2022-03-21 20:13:27 -0400

    regex fun :)

--- JS/6 kyu/problems.js
+++ JS/6 kyu/problems.js
@@ -1,3 +1,10 @@
-// 5 kyu | Primes in numbers
+// 6 kyu | Your order, please
 
-function primeFactors(n) {}
+function order(words) {
+  return words.split(" ").sort((a, b) => a.match(/\d/) - b.match(/\d/)).join(" ");
+}
+
+words = "Thi2s i5s 3a t7est se9ntence ok1ay";
+console.log(order(words));
+
+// split into array, regex to find digit, sort by number. tada!
