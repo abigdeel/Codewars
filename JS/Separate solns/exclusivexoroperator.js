bc877e4a4343e5085dd560bd78b32538b245c543
Adeel Syed <adeel99@gmail.com>
2022-07-14 12:00:00 -0400

    8 Kyu | Exclusive "or" operator xor

---
+++ JS/Separate solns/exclusivexoroperator.js
@@ -0,0 +1,7 @@
+// 8 Kyu | Exclusive "or" operator xor
+
+function xor(a, b) {
+  if (a == false && b == false) return false;
+  else if (a == true && b == true) return false;
+  else return true;
+}
