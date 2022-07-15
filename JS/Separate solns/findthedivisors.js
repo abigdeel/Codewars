0b2bf7ac2d0e25b6ca149e2c2e6bb3813b097956
Adeel Syed <adeel99@gmail.com>
2022-07-15 12:00:00 -0400

    7 Kyu | Find the divisors

---
+++ JS/Separate solns/findthedivisors.js
@@ -0,0 +1,9 @@
+// 7 Kyu | Find the divisors
+
+function divisors(integer) {
+  let arr = [];
+  for (i = 2; i < integer; i++) {
+    if (integer % i == 0) arr.push(i);
+  }
+  return arr.length == 0 ? `${integer} is prime` : arr;
+}
