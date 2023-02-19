4411d1c5bd8f2428e48c247805cf225c28fa5544
Adeel Syed <adeel99@gmail.com>
2023-02-19 10:01:43 -0500

    7 Kyu | Credit Card Mask

--- JS/Separate solns/convertstringtonum.js
+++ JS/Separate solns/convertstringtonum.js
@@ -1,3 +1,10 @@
-var stringToNumber = function (str) {
-  return parseInt(str);
-};
+// 7 Kyu | Credit Card Mask
+
+function maskify(cc) {
+  return cc
+    .split("")
+    .map((char, index) => {
+      return index < cc.length - 4 ? "#" : char;
+    })
+    .join("");
+}
