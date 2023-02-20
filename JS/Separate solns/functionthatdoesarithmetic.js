5e92f328d09e1b63b64d8e21fd09f66e04733ff1
Adeel Syed <adeel99@gmail.com>
2023-02-20 11:25:02 -0500

    // 7 Kyu | Make a function that does arithmetic

---
+++ JS/Separate solns/functionthatdoesarithmetic.js
@@ -0,0 +1,14 @@
+// 7 Kyu | Make a function that does arithmetic
+
+function arithmetic(a, b, operator) {
+  switch (operator) {
+    case "add":
+      return a + b;
+    case "subtract":
+      return a - b;
+    case "multiply":
+      return a * b;
+    case "divide":
+      return a / b;
+  }
+}
